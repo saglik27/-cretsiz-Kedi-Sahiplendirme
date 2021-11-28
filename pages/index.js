1eb4aab73a3eab2ab786f5f9488652b3a6291faa
saglik27 <87330610+saglik27@users.noreply.github.com>
2021-11-28 14:47:36 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Başlamak için AnyDesk'i açın. Solda gördüğünüz numara kişisel kimliğinizdir. Kişiler cihazınıza erişmek için bu numarayı kullanabilir ve bu numaraya bağlanmak istediğiniz kişiden ihtiyacınız vardır. Sağdaki arama çubuğuna uzak cihazın kimliğini yazın. Unutmayın: Tanımadığınız hiç kimsenin cihazınıza erişmesine izin vermeyin ve çevrimiçi dolandırıcılara karşı dikkatli olun !
-
-Ya evdeyseniz, ancak oturum isteğini kabul edecek kimsenin olmadığı ofiste iş bilgisayarınıza gözetimsiz olarak erişmek istiyorsanız? Güvenlik ayarlarınıza bir göz atın: Katılımsız erişim için bir parola ve iki faktörlü kimlik doğrulama ayarlayabilirsiniz . Bu şekilde, oturumun uzak taraf tarafından manuel olarak kabul edilmesi gerekmez." />
+        <Header title="istiyorsanız? Güvenlik ayarlarınıza bir göz atın: Katılımsız erişim için bir parola ve iki faktörlü kimlik doğrulama ayarlayabilirsiniz . Bu şekilde, oturumun uzak taraf tarafından manuel olarak kabul edilmesi gerekmez." />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editihhhng <code>pages/index.js</code>
         </p>
       </main>
 
