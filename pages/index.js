4ba715801ee99d8b1804cf281a7a22858bdad5e3
saglik27 <87330610+saglik27@users.noreply.github.com>
2021-11-28 14:56:21 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,9 @@ export default function Home() {
       <main>
         <Header title="Anydesk nedir - ne işe yarar - anydesk dosya aktarımı" />
         <p className="description">
-          Get started by editihhhng <code>pagehhhs/index.js</code>
+          Başlamak için AnyDesk'i açın. Solda gördüğünüz numara kişisel kimliğinizdir. Kişiler cihazınıza erişmek için bu numarayı kullanabilir ve bu numaraya bağlanmak istediğiniz kişiden ihtiyacınız vardır. Sağdaki arama çubuğuna uzak cihazın kimliğini yazın. Unutmayın: Tanımadığınız hiç kimsenin cihazınıza erişmesine izin vermeyin ve çevrimiçi dolandırıcılara karşı dikkatli olun !
+
+Ya evdeyseniz, ancak oturum isteğini kabul edecek kimsenin olmadığı ofiste iş bilgisayarınıza gözetimsiz olarak erişmek istiyorsanız? Güvenlik ayarlarınıza bir göz atın: Katılımsız erişim için bir parola ve iki faktörlü kimlik doğrulama ayarlayabilirsiniz . Bu şekilde, oturumun uzak taraf tarafından manuel olarak kabul edilmesi gerekmez.
         </p>
       </main>
 
