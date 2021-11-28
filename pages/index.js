756d26c0ab3272165149fecaa338b9243e1cdbd7
saglik27 <87330610+saglik27@users.noreply.github.com>
2021-11-28 15:04:38 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,10 @@ export default function Home() {
         <p className="description">
           Başlamak için AnyDesk'i açın. Solda gördüğünüz numara kişisel kimliğinizdir. Kişiler cihazınıza erişmek için bu numarayı kullanabilir ve bu numaraya bağlanmak istediğiniz kişiden ihtiyacınız vardır. Sağdaki arama çubuğuna uzak cihazın kimliğini yazın. Unutmayın: Tanımadığınız hiç kimsenin cihazınıza erişmesine izin vermeyin ve çevrimiçi dolandırıcılara karşı dikkatli olun !
 
-Ya evdeyseniz, ancak oturum isteğini kabul edecek kimsenin olmadığı ofiste iş bilgisayarınıza gözetimsiz olarak erişmek istiyorsanız? Güvenlik ayarlarınıza bir göz atın: Katılımsız erişim için bir parola ve iki faktörlü kimlik doğrulama ayarlayabilirsiniz . Bu şekilde, oturumun uzak taraf tarafından manuel olarak kabul edilmesi gerekmez. kaynak: https://takdio.com/anydesk-nedir-nasil-kullanilir/
+Ya evdeyseniz, ancak oturum isteğini kabul edecek kimsenin olmadığı ofiste iş bilgisayarınıza gözetimsiz olarak erişmek istiyorsanız? Güvenlik ayarlarınıza bir göz atın: Katılımsız erişim için bir parola ve iki faktörlü kimlik doğrulama ayarlayabilirsiniz . Bu şekilde, oturumun uzak taraf tarafından manuel olarak kabul edilmesi gerekmez. kaynak: <a href="https://takdio.com/anydesk-nedir-nasil-kullanilir">https://takdio.com/anydesk-nedir-nasil-kullanilir</a>/
         </p>
     <body>
-    deneme
+    <iframe src="https://takdio.com/wp-content/uploads/2021/07/Uzak-Masaustu-baglantisi-Nasil-yapilir.png"></iframe>
     </body>
     
       </main>
